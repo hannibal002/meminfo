fa45d2fbb991ed45d068e154000df935b6fdc0f8
Manuel Kollus <officialragecode@gmail.com>
2019-05-29 20:01:28 +0200

    Make the MemoryInfoKind to enumeration

--- meminfo-unit/src/main/java/io/github/manuelkollus/meminfo/unit/MemoryInfoKind.java
+++ meminfo-unit/src/main/java/io/github/manuelkollus/meminfo/unit/MemoryInfoKind.java
@@ -7,15 +7,32 @@ package io.github.manuelkollus.meminfo.unit;
  * @version 1.0
  * @see MemoryInfoFileField
  */
-public final class MemoryInfoKind {
+public enum MemoryInfoKind {
 
-  private String name;
+  MEM_TOTAL("MemTotal", MemoryInfoUnit.KILO_BYTES),
+  MEM_FREE("MemFree", MemoryInfoUnit.KILO_BYTES),
+  MEM_AVAILABLE("MemAvailable", MemoryInfoUnit.KILO_BYTES),
+  BUFFERS("Buffers", MemoryInfoUnit.KILO_BYTES),
+  CACHED("Cached", MemoryInfoUnit.KILO_BYTES),
+  SWAP_CACHED("SwapCached", MemoryInfoUnit.KILO_BYTES),
+  ACTIVE("Active", MemoryInfoUnit.KILO_BYTES),
+  INACTIVE("Inactive", MemoryInfoUnit.KILO_BYTES);
 
-  private MemoryInfoKind(String name) {
-    this.name = name;
+  private final String key;
+  private final MemoryInfoUnit memoryInfoUnit;
+
+  MemoryInfoKind(
+    String key,
+    MemoryInfoUnit memoryInfoUnit) {
+    this.key = key;
+    this.memoryInfoUnit = memoryInfoUnit;
   }
 
-  public String name() {
-    return name;
+  public String key() {
+    return key;
+  }
+
+  public MemoryInfoUnit memoryInfoUnit() {
+    return memoryInfoUnit;
   }
 }
