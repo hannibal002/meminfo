e49f08f68728ea820e9ef02950b4441f1a601cb9
qubeSector <officialragecode@gmail.com>
2019-05-30 10:58:18 +0200

    Add missing documentation

--- meminfo-parser/src/main/java/io/github/manuelkollus/meminfo/parser/MemoryInfoParserFactory.java
+++ meminfo-parser/src/main/java/io/github/manuelkollus/meminfo/parser/MemoryInfoParserFactory.java
@@ -25,11 +25,22 @@ public final class MemoryInfoParserFactory {
   private final Path path;
   private final MemoryInfoParser memoryInfoParser;
 
+  /**
+   * Private constructor of the {@link MemoryInfoFileField} class.
+   *
+   * @param path memoryinfo file to build the {@link MemoryInfoFileField}
+   */
   private MemoryInfoParserFactory(Path path) {
     this.path = path;
     this.memoryInfoParser = new MemoryInfoParser();
   }
 
+  /**
+   * Returns an list of {@link MemoryInfoFileField}. Their are iterate all lines
+   * from the target memory info file to build the {@link MemoryInfoFileField}.
+   *
+   * @return an list of {@link MemoryInfoFileField}.
+   */
   public List<MemoryInfoFileField> getFieldsFromFile() throws IOException {
     Objects.requireNonNull(path, "path cannot be null!");
 
@@ -39,15 +50,31 @@ public final class MemoryInfoParserFactory {
       .collect(Collectors.toList());
   }
 
+  /**
+   * Return an parsed line to {@link MemoryInfoFileField}.
+   *
+   * @return an parsed line to {@link MemoryInfoFileField}.
+   */
   private Function<String, MemoryInfoFileField> parseLine() {
     return line -> line.contains("kB") ? memoryInfoParser.parseWithUnit(line) :
       memoryInfoParser.parseWithoutUnit(line);
   }
 
+  /**
+   * Creates a new instance of the {@code MemoryInfoParserFactory} class.
+   *
+   * @return a new instance of the {@code MemoryInfoParserFactory} class.
+   */
   public static MemoryInfoParserFactory create() {
     return new MemoryInfoParserFactory(PATH);
   }
 
+  /**
+   * Creates a new instance of the {@code MemoryInfoParserFactory} class.
+   *
+   * @param path memoryinfo file to build the {@link MemoryInfoFileField}
+   * @return a new instance of the {@code MemoryInfoParserFactory} class.
+   */
   public static MemoryInfoParserFactory create(Path path) {
     Objects.requireNonNull(path, "path cannot be null!");
 
